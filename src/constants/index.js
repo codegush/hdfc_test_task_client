d2f223b1f4a353342bd858790b1a2fd54a5fbbd6
Hemant <hemantmanwani.iitr@gmail.com>
2018-05-01 10:57:09 +0530

    Add production API_BASE_URL

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
-export const API_BASE_URL = 'https://localhost:3000/api/v1';
+const env = process.env.NODE_ENV
+
+export const API_BASE_URL = env === 'production' ? ' https://hdfcapi.herokuapp.com/api/v1' : 'https://localhost:3000/api/v1';
+
 
 export const GOOGLE_AUTH_PATH = '/auth/google_oauth2';
 export const FACEBOOK_AUTH_PATH = '/auth/facebook';
@@ -6,3 +9,4 @@ export const LINKEDIN_AUTH_PATH = '/auth/linkedin';
 export const GITHUB_AUTH_PATH = '/auth/github';
 export const INSTAGRAM_AUTH_PATH = '/auth/instagram';
 export const TWITTER_AUTH_PATH = '/auth/twitter';
+
